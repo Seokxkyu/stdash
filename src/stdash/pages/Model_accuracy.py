5cbd511c5c3d56e0e8aafd802b6c94fa0833550d
Seokxkyu <kyuseok000@gmail.com>
2024-09-27 12:47:47 +0900

    11

--- src/stdash/pages/Model_accuracy.py
+++ src/stdash/pages/Model_accuracy.py
@@ -3,10 +3,13 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
+# Set up the Streamlit app configuration
 st.set_page_config(page_title="Model Accuracy Statistics", page_icon="📈")
 
+# Title of the app
 st.title('Model Accuracy Statistics')
 
+# Function to load the data from the API
 def load_data():
     url = 'http://43.202.66.118:8077/all'  # Replace with your API URL
     r = requests.get(url)
@@ -18,10 +21,9 @@ df = pd.DataFrame(data)
 
 df['request_time'] = pd.to_datetime(df['request_time'])
 df['prediction_time'] = pd.to_datetime(df['prediction_time'])
-
 df['correct'] = df['label'] == df['prediction_result']
 
-accuracy_stats = df.groupby(['request_user', 'prediction_model']).agg(
+accuracy_stats = df.groupby('prediction_model').agg(
     total_predictions=('correct', 'size'),
     correct_predictions=('correct', 'sum')
 )
@@ -31,14 +33,13 @@ accuracy_stats = accuracy_stats.reset_index()
 
 st.dataframe(accuracy_stats)
 
-st.header('Accuracy per User and Model')
+st.header('Accuracy per Model')
 
 plt.figure(figsize=(12, 8))
-pivot_table = accuracy_stats.pivot(index='request_user', columns='prediction_model', values='accuracy')
-ax = pivot_table.plot(kind='barh', stacked=True, figsize=(12, 8))
+ax = accuracy_stats.plot(kind='bar', x='prediction_model', y='accuracy', legend=False, figsize=(12, 8))
 
-plt.title('Accuracy per User and Model')
-plt.xlabel('Accuracy')
-plt.ylabel('Request User')
+plt.title('Model Accuracy')
+plt.xlabel('Prediction Model')
+plt.ylabel('Accuracy')
 
 st.pyplot(plt)
