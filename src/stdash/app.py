b2f7e88194e0bcaba4e4240df88529955079271e
Seokxkyu <kyuseok000@gmail.com>
2024-09-27 12:41:12 +0900

    app

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -13,30 +13,18 @@ def load_data():
 
 data = load_data()
 df = pd.DataFrame(data)
-'''
-df['request_time'] = pd.to_datetime(df['request_time'])
-df['req_time'] = df['request_time'].dt.strftime("%Y-%m-%d %H")
-df_req = df.groupby('req_time').count()
-df_req
+
+df
+
 
 df['prediction_time'] = pd.to_datetime(df['prediction_time'])
 df['pred_time'] = df['prediction_time'].dt.strftime("%Y-%m-%d %H")
-df_pred = df.groupby('pred_time').count()
-df_pred
-
-st.header('요청 / 처리 건수(h)')
-plt.bar(df_req.index, df_req['num'])
-plt.plot(df_pred.index, df_pred['num'], 'ro-')
-plt.title('Requests by Date and Time')
-plt.xlabel('Date and Time')
-plt.ylabel('Number of Requests')
-plt.xticks(rotation = 45)
-st.pyplot(plt)
-'''
 
 df['request_time'] = pd.to_datetime(df['request_time'])
 df['request_time']=df['request_time'].dt.strftime('%Y-%m-%d %H')
+
 count=df.groupby('request_time').size()
+
 plt.bar(count.index,count.values)
 plt.plot(count.index,count.values,color='red',linestyle='-',marker='o')
 plt.plot(count.index,count.values,color='red',linestyle='-',marker='o')
