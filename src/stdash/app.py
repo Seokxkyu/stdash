0a36e7679781069fa358b3a991c49b500dcf8c3b
Seokxkyu <kyuseok000@gmail.com>
2024-09-27 15:09:02 +0900

    env variables

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -6,7 +6,9 @@ import requests
 st.title('CNN Model build')
 
 def load_data():
-    url = 'http://43.202.66.118:8077/all'
+    DB = os.getenv('DB')
+    DB_PORT =os.getenv('DB_PORT')
+    url = f'http://{DB}:{DB_PORT}/all'
     r = requests.get(url)
     d = r.json()
     return d
